b9aa067391f9abdcaafb4859e0d7a266b642cd95
Joshua Huston <joshua.david.huston@gmail.com>
2022-03-26 19:51:16 -0400

    Changed Database Engine to Postgres

--- project/settings.py
+++ project/settings.py
@@ -101,7 +101,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     "default": {
-        "ENGINE": env.str("DATABASE_ENGINE"),
+        "ENGINE": 'django.db.backends.postgresql',
         "NAME": env.str("DATABASE_NAME"),
         "USER": env.str("DATABASE_USER"),
         "PASSWORD": env.str("DATABASE_PASSWORD"),
